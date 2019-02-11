d700fd75df337ae65827cc027eccfe8df8d60b19
Tobi Akerele <otakerele@gmail.com>
2019-02-11 14:51:09 -0500

    listen to queue, get mac address

--- rfid.py
+++ rfid.py
@@ -1,9 +1,8 @@
+#!/usr/bin/env python
+
 # Table Name - rfid_data
 # Fields - id, rfid_tag, time, count
 
-#!/usr/bin/env python
-from tkinter import *
-import pymysql.cursors
 import time
 import serial
 import binascii
@@ -11,13 +10,15 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import sched
+import uuid
 
 """
 Logic
-When an RFID is scanned, first check the database for the case ID of the caseRFIDs table.
+When an RFID is scanned, first check the database for the case ID of the caseRFIDs table. DONE
 Check if the case has a queue, if it does, check from caseQueue, if it doesn't, go to completeOrders
 Check table and get the most recent push or position. 
 """
+
 
 class Database:
     def __init__(self):
@@ -25,62 +26,71 @@ class Database:
         self.dbApp = firebase_admin.initialize_app(self.cred)
 
     def update(self, path, data):
-        db.reference(path).update(data)
+        return db.reference(path).update(data)
 
     def delete(self, path):
-        db.reference(path).delete()
+        return db.reference(path).delete()
 
     def set(self, path, data):
-        db.reference(path).set(data)
+        return db.reference(path).set(data)
 
     def get(self, path):
-        db.reference(path).get()
+        return db.reference(path).get()
 
     def push(self, path, data):
-        db.reference(path).push(data)
+        return db.reference(path).push(data)
 
     def listen(self, path):
-        def callback(change):
-            print(change)
-        db.reference(path).listen(callback)
+        def listener(event):
+            print(event.event_type)  # can be 'put' or 'patch'
+            print(event.path)  # relative to the reference
+            print(event.data)  # new data at /reference/event.path. None if deleted
+        return db.reference(path).listen(listener)
 
 
-def view(rfidvalue):
-    if rfidvalue[0:8] == "02000015":
-        print(rfidvalue)
+def get_caseid_with_rfid(database, rfid):
+    # Pass in the table where the RFID and caseID relationship is stored
+    # Returns the caseID as a string
+    return database.get('caseRFIDs/{0}'.format(rfid))
 
 
-def startscanning(): #function that scan rfid
+# Continuously scans rfid
+def start_scanning(db):
     size = ser.inWaiting()
     if size:
-        x = ser.read(size)
-        time.sleep(1)
-        x = binascii.hexlify(x)
-        q = x.decode("ascii")  #converting scanned data
-        print(q[4:27])
-        rfidvalue = q[4:27]
-        view(rfidvalue)
-    else:
-        print('Scanning...')
-    s.enter(1, 1, startscanning)
+        rfid_value = convert_scan(size)
+        print(rfid_value)
+        case_id = get_caseid_with_rfid(db, rfid_value)
+    s.enter(1, 1, start_scanning, firedb)
 
-rfidtag =[]
-# class RFIDReads:
+
+# Reads the size byte, converts it to hex then decodes to ascii
+def convert_scan(size):
+    x = ser.read(size)
+    time.sleep(1)
+    x = binascii.hexlify(x)
+    q = x.decode("ascii")
+    return q[4:27]
+
 
 if __name__ == "__main__":
 
-    # connect to database sql
+    # Create an instance of the database class
     firedb = Database()
-    
-    # ---------------------
-    # Open Serial----------
+    # Get the MAC Address of the device running program. Used to uniquely identify each kart
+    mac_id = hex(uuid.getnode())
+
+    # Open Serial, if there is an exception, try the next port
     try: 
         ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.0001)
-    except:
+    except serial.SerialException as msg:
         ser = serial.Serial(port='/dev/ttyUSB1', baudrate=9600, timeout=.0001)
- 
-    # ---------------------
 
     s = sched.scheduler(time.time, time.sleep)
-    s.enter(1, 1, startscanning)
+    # Set a delay of 1 second with priority 1, pass in the scanning function and the firedb instance as an argument
+    s.enter(1, 1, start_scanning, firedb)
     s.run()
+
+    # TODO: Give each raspberry pi a unique id by using something like their mac address
+    # Listen to the kartQueue table for any orders that come in
+    firedb.listen('kartQueue')
